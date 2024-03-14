700dd96f41d216ffc9595c34fe9cd56fcf31f17d
Ehsan Hussain <ehsan-1995@hotmail.com>
2024-03-14 21:42:46 +0100

    update

--- Taxi Landing Page/script.js
+++ Taxi Landing Page/script.js
@@ -1,36 +1,29 @@
 window.addEventListener('hashchange', function() {
-    
-    var pageName = window.location.hash.substr(1);
+  var pageName = window.location.hash.substr(1);
+  loadPage(pageName);
+});
 
-   
-    loadPage(pageName);
-  });
-
-  function loadPage(pageName) {
-   
-    var pageMappings = {
+function loadPage(pageName) {
+  var pageMappings = {
       services: 'component/services.html',
       technology: 'component/technology.html',
       who_we_serve: 'component/who_we_serve.html',
       pricing: 'component/pricing.html',
-    };
-    
+      contact: 'component/contact.html',
+  };
 
-    var pageUrl = pageMappings[pageName] || 'default.html'; 
+  var pageUrl = pageMappings[pageName] || 'default.html';
 
-
-    fetch(pageUrl)
+  fetch(pageUrl)
       .then(response => response.text())
       .then(html => {
-        document.querySelector('.container').innerHTML = html;
+          document.getElementById('main-content').innerHTML = html;
       })
       .catch(error => {
-        console.error('Failed to load the page:', error);
+          console.error('Failed to load the page:', error);
       });
-  }
+}
 
-
-  if(window.location.hash) {
-  
-    loadPage(window.location.hash.substr(1));
-  }
+if (window.location.hash) {
+  loadPage(window.location.hash.substr(1));
+}
